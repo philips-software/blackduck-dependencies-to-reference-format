f4b3fe3b82ba8c3db8ca04825be412351a7a0b15
Sanda Contiu <sanda.contiu@philips.com>
2019-11-04 12:16:05 +0100

    updated version of this package to 1.0.1 (incl. documentation)

--- src/cli-extract-from-source.js
+++ src/cli-extract-from-source.js
@@ -19,7 +19,7 @@ const { getAsyncJsonArrayFromCsv } = require('./file-readers/read-csv-to-json-ar
 const { hasFileExtension } = require('./file-validators/file-extension-validator')
 
 program
-  .version('0.0.1', '-v, --version')
+  .version('1.0.1', '-v, --version')
   .option(
     '-i, --input [file]',
     '(mandatory) specifies source.csv filename which contains the dependencies as identified by Synopsis Detect'
